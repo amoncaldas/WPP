4dceb126f2f4b3999326625fb7ebb418b2cd32ff
Amon <amoncaldas@yahoo.com.br>
2019-03-19 12:56:44 -0300

    Added offline fonts files

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -16,6 +16,12 @@
 </template>
 <script src="./app.js"></script>
 
+<!--<style lang="css" src="@/assets/css/font-roboto.css"></style>
+<style lang="css" src="@/assets/css/font-raleway.css"></style>
+<style lang="css" src="@/assets/css/font-open-sans.css"></style>
+<style lang="css" src="@/assets/css/material-icons.css"></style>
+<style lang="css" src="@/assets/css/animate.min.css"></style>-->
+
 <style lang="scss" src="./app.scss"></style>
 
 
