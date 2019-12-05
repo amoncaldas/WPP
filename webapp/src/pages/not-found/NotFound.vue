64460e3fc75feab926c7f7a427b26045bfc3f633
Amon <amoncaldas@yahoo.com.br>
2019-12-05 13:23:13 -0300

    Changed page route strategy to be catch in not found route

--- webapp/src/pages/not-found/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container text-xs-center fluid-xs class="page-root not-found">
-    <v-slide-y-transition mode="out-in">
-      <not-found-component></not-found-component>
-    </v-slide-y-transition>
-  </v-container>
-</template>
-
-<script src="./not-found.js"></script>
---
+++ webapp/src/pages/page-or-not-found/i18n/page-or-not-found.i18n.en.js
@@ -0,0 +1,5 @@
+export default {
+  pageOrNotFound: {
+    notFound: 'Not found'
+  }
+}
