9ef5e563c8095efefa9f114c2ff35c18c1f0dae4
Amon Caldas <amoncaldas@gmail.com>
2021-04-15 15:41:49 +0200

    Added new VRC

--- webapp/src/fragments/contact-form/ContactForm.vue
+++ webapp/src/fragments/contact-form/ContactForm.vue
@@ -5,39 +5,46 @@
         <h2>{{$t('contactForm.title')}}</h2>
       </div>
       <div slot="content">
-        <v-form ref="form">
-          <v-text-field :label="$t('contactForm.subject')" v-model="resource.subject" autofocus required></v-text-field>
-          <v-text-field
-            :label="$t('contactForm.name')"
-            ref="name"
-            class="name"
-            v-model="resource.name"
-            :counter="20">
-          </v-text-field>
-          <v-text-field
-            ref="email"
-            :label="$t('contactForm.email')"
-            class="user-email"
-            v-model="resource.email"
-            :rules="emailRules"
-            required>
-          </v-text-field>
-          <v-textarea auto-grow class="contact-message" :height="$vuetify.breakpoint.mdAndUp ? 300 : 150" type="text" hide-details counter
-            :label="$t('contactForm.yourMsg')" box
-            :title="$t('contactForm.yourMsg')"
-            v-model="resource.message"
-            :required="true">
-          </v-textarea>
-          <div class="contact-btn-container" v-if="ready">
-            <v-btn color="secondary" left @click.native="submit">{{$t('global.send')}}</v-btn>
+        <vrc-form 
+          ref="vrcForm"
+          :vm="_self"
+          :sendTitle="$t('global.send')" 
+          :httpOptions="httpOptions"
+          
+          :crud-options="vrcOptions"
+          mode="create"
+          :content-name="$t('contactForm.yourMsg')"
+          endpoint="wpp/v1/message/send">
+          <div slot="default" v-if="crudReady">
+            <v-text-field :label="$t('contactForm.subject')" 
+              v-model="resource.subject" 
+              autofocus 
+              required>
+            </v-text-field>
+            <v-text-field
+              :label="$t('contactForm.name')"
+              ref="name"
+              class="name"
+              v-model="resource.name"
+              required
+              :counter="20">
+            </v-text-field>
+            <v-text-field
+              ref="email"
+              :label="$t('contactForm.email')"
+              class="user-email"
+              v-model="resource.email"
+              :rules="emailRules"
+              required>
+            </v-text-field>          
+            <v-textarea auto-grow class="contact-message" :height="$vuetify.breakpoint.mdAndUp ? 300 : 150" type="text" hide-details counter
+              :label="$t('contactForm.yourMsg')" box
+              :title="$t('contactForm.yourMsg')"
+              v-model="resource.message"
+              :required="true">
+            </v-textarea>
           </div>
-          <vue-recaptcha v-if="$store.getters.options.recaptcha_site_key" :sitekey="$store.getters.options.recaptcha_site_key"
-            ref="recaptcha"
-            size="invisible"
-            @verify="onCaptchaVerified"
-            @expired="onCaptchaExpired">
-          </vue-recaptcha>
-        </v-form>
+        </vrc-form>
       </div>
     </box>
   </div>
--- webapp/src/fragments/contact-form/contact-form.js
+++ webapp/src/fragments/contact-form/contact-form.js
@@ -1,68 +1,32 @@
-import contactService from './contact-form-service'
-import VueRecaptcha from 'vue-recaptcha'
-import VueRestClient from 'vue-rest-client'
+import {VrcForm, CrudData} from 'vue-rest-client'
 import pattern from '@/support/pattern'
+import HttpClientOptions from '@/common/http-client-options'
 
 export default {
   name: 'contact-form',
-  created () {
-    // extend this component, adding CRUD functionalities and load the tokens
-    let options = {
-      queryOnStartup: false,
-      skipAutoIndexAfterAllEvents: true,
-      resourceSavedMsg: this.$t('contactForm.msgSent'),
-      failWhileTryingToSaveResourceMsg: this.$t('contactForm.sendErrorMsg')
-    }
-    VueRestClient.Controller.set(this, contactService, options)
-  },
-  data () {
-    return {
-      ...VueRestClient.CrudData, // adds: resource, resources, crudReady and modelService
-      verifiedCaptcha: false,
-      resource: {},
-      context: null,
-      ready: true
-    }
-  },
   computed: {
+    vrcOptions () {
+      return {
+        queryOnStartup: false,
+        skipAutoIndexAfterAllEvents: true,
+        resourceSavedMsg: this.$t('contactForm.msgSent'),
+        failWhileTryingToSaveResourceMsg: this.$t('contactForm.sendErrorMsg')
+      }
+    },
+    httpOptions () {
+      let options = HttpClientOptions
+      return options
+    },
     emailRules () {
       return [ !!this.resource.email && pattern.email.test(this.resource.email) || this.$t('contactForm.pleaseTypeAValidEmail') ]
     }
   },
-  methods: {
-    sendMsg () {
-      this.save()
-    },
-    submit () {
-      this.showInfo(this.$t('contactForm.processingYourMsg'), {timeout: 6000})
-      if (this.$store.getters.options.recaptcha_site_key) {
-        this.$refs.recaptcha.execute()
-      } else {
-        this.sendMsg()
-      }
-    },
-    onCaptchaVerified (recaptchaToken) {
-      this.showInfo(this.$t('contactForm.processingYourMsg'), {timeout: 6000})
-      this.resource.recaptchaToken = recaptchaToken
-      const self = this
-      self.$refs.recaptcha.reset()
-      self.verifiedCaptcha = true
-      this.sendMsg()
-    },
-    onCaptchaExpired () {
-      this.$refs.recaptcha.reset()
-      this.verifiedCaptcha = false
-    }
-  },
-  mounted () {
-    if (this.$store.getters.options.recaptcha_site_key) {
-      this.ready = false
-      this.loadRecaptcha().then(() => {
-        this.ready = true
-      })
+  data() {
+    return {
+      ...CrudData, // adds: resource, resources, crudReady and modelService
     }
   },
   components: {
-    VueRecaptcha
+    VrcForm
   }
 }
